3ac06525fe358c9d109a42ac3de4cc30f0a39bbc
Gabriela Alves da Gama <gabrielaalvesdagama@gmail.com>
2017-09-21 20:57:08 -0300

    Adding new method comments in componentMovement file

--- src/componentMovement.cpp
+++ src/componentMovement.cpp
@@ -1,10 +1,28 @@
+/*!
+ *  \file File componentMovement.cpp
+ *  \brief Implementation of the class of component movements present in the game
+ *
+ *  The class implemented here provides to the game the speed and type
+ *  of movement that a given element can perform
+ *
+ *  \sa componentMovement.h
+ */
+
 #include <componentMovement.hpp>
 #include <gameObject.hpp>
 #include <game.hpp>
 //#include <camera.hpp>
 //#include <inputManager.hpp>
 
+//! A constructor.
+    /*!
+    This is a constructor method os componentMovement class
+    */
 CompMovement::CompMovement(const Vec2& s,moveType t):mType{t},speed{s}{}
+//! A destructor.
+    /*!
+      This is a destructor method os componentMovement class
+    */
 CompMovement::~CompMovement() {}
 
 
