78ee97c2a1df0b0f054dd5ed3862b81acc3df8be
Gabriela Alves da Gama <gabrielaalvesdagama@gmail.com>
2017-09-22 00:04:03 -0300

    Adding method comments componentTimer.cpp

--- src/componentTimer.cpp
+++ src/componentTimer.cpp
@@ -2,7 +2,7 @@
  *  \file File componentTimer.cpp
  *  \brief Implementation of the class of game time
  *
- *  The class implemented here provides to the game the time limit 
+ *  The class implemented here provides to the game the time limit
  *
  *  \sa componentTimer.hpp
  */
@@ -14,9 +14,27 @@
 //#include <camera.hpp>
 //#include <inputManager.hpp>
 
-
+//! A constructor.
+    /*!
+    This is a constructor method of componentTimer class
+    */
 CompTimer::CompTimer(float l):limit{l}{}
+
+//! A destructor.
+    /*!
+      This is a destructor method of componentText class
+    */
+
 CompTimer::~CompTimer() {}
+
+/*!
+	@fn CompTimer::Update(float time)
+	@brief Method that update the game time
+	and check the time limit
+	@param time
+	@return The execution of this method returns no value
+	@warning Method that requires review of comment
+*/
 
 
 void CompTimer::Update(float time) {
@@ -26,13 +44,35 @@ void CompTimer::Update(float time) {
 	}
 }
 
+/*!
+	@fn void CompTimer::Render()
+	@brief Method that render the game time
+	@return The execution of this method returns no value
+	@warning Method that requires review of comment
+*/
+
 
 void CompTimer::Render() {}
+
+/*!
+	@fn void CompTimer::Own(GameObject* go)
+	@brief Method that monitor the game element
+	@param GameObject* go
+	@return The execution of this method returns no value
+	@warning Method that requires review of comment
+*/
 
 
 void CompTimer::Own(GameObject* go) {
 	entity=go->uid;
 }
+
+/*!
+	@fn void CompMovement::Update(float time)
+	@brief Method that sets the game time
+	@return The method return the current time
+	@warning Method that requires review of comment
+*/
 
 
 Component::type CompTimer::GetType() const{
