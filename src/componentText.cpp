80111ca8fdf6691d85e72b2957d500b46fe9532e
Gabriela Alves da Gama <gabrielaalvesdagama@gmail.com>
2017-09-21 23:43:51 -0300

    Adding method comments componentText.cpp

--- src/componentText.cpp
+++ src/componentText.cpp
@@ -13,18 +13,49 @@
 #include <game.hpp>
 //#include <inputManager.hpp>
 
+//! A constructor.
+    /*!
+    This is a constructor method of componentText class
+    */
 CompText::CompText(const Text &t,Hotspot h,Vec2 p):txt{t},pos{p}{
 	txt.SetHotspot(h);
 }
+
+//! A constructor.
+    /*!
+    This is a another constructor method of componentText class
+		with overload
+    */
 CompText::CompText(string text,int size,SDL_Color c,Hotspot h,Vec2 p):txt{text,size,c},pos{p}{
 	txt.SetHotspot(h);
 }
+
+//! A destructor.
+    /*!
+      This is a destructor method of componentText class
+    */
+
 CompText::~CompText() {}
 
+/*!
+	@fn void CompMovement::Update(float time)
+	@brief Method that update element movement
+	@param time
+	@return The execution of this method returns no value
+	@warning Method that requires review of comment
+*/
 
 void CompText::Update(float time) {
 	UNUSED(time);
 }
+
+/*!
+	@fn void CompText::Render()
+	@brief Method that render the text messege
+	@return The execution of this method returns no value
+	@warning Method that requires review of comment
+*/
+
 void CompText::Render() {
 	Vec2 p = pos + GO(entity)->Box().corner();
 	txt.SetPos(p);
@@ -32,6 +63,14 @@ void CompText::Render() {
 	if (GO(entity)->anchored) txt.Render();
 	else txt.Render(CAMERA);
 }
+
+/*!
+	@fn Component::type CompText::GetType()const
+	@brief Method that sets the text messege
+	@return the type of the text messege
+	@warning Method that requires review of comment
+*/
+
 Component::type CompText::GetType()const{
 	return Component::type::t_text;
 }
