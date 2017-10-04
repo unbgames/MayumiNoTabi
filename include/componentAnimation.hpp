3ac3352bc711524fc99f1aabb7c5e2ee6411645b
André de Sousa Costa filho <dedaoskater@hotmail.com>
2017-10-04 10:40:22 -0300

    makes some changes

--- include/componentAnimation.hpp
+++ include/componentAnimation.hpp
@@ -8,31 +8,31 @@
 #include <sprite.hpp>
 #include <componentCollider.hpp>
 
-#define COMPANIM(x)  ((CompAnim*)x.components[Component::type::t_animation])
-#define COMPANIMPOINTER(x) ((CompAnim*)x->components[Component::type::t_animation])
+#define COMPANIM(x)  ((CompAnim*)x. components[Component::type::t_animation])
+#define COMPANIMp(x) ((CompAnim*)x->components[Component::type::t_animation])
 
 class CompAnim : public Component{
 public:
-  vector<CompCollider*> colliders;
-  map<int,vector<function<void(GameObject*)>>> frameFunc;
-  Sprite sp;
-  bool called=true;
+	vector<CompCollider*> colliders;
+	map<int,vector<function<void(GameObject*)>>> frameFunc;
+	Sprite sp;
+	bool called=true;
 
-  //public members
-  CompAnim();
-  CompAnim(string filename, CompCollider* temporary_collider);
-  ~CompAnim();
+	//public members
+	CompAnim();
+	CompAnim(string file,CompCollider* coll);
+	~CompAnim();
 
-  int get_frame_count() const;
-  int get_current_frame() const;
-  void set_current_frame(int frame, bool force=false);
+	int get_frame_count()const;
+	int get_current_frame()const;
+	void set_current_frame(int frame, bool force=false);
 
-  bool is_looped()const;
+	bool Looped()const;
 
-  void update(float time);
-  void own(GameObject *game_object);
-  void render();
-  Component::type get_type()const;
+	void Update(float time);
+	void own(GameObject *go);
+	void Render();
+	Component::type GetType()const;
 };
 
 #endif
