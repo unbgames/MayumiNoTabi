d9e6452a3227da586d0bed1fd3e71b1cae33ff35
Gabriela Alves da Gama <gabrielaalvesdagama@gmail.com>
2017-10-09 11:27:13 -0300

    Applying stylesheet in componentText.cpp

--- src/componentText.cpp
+++ src/componentText.cpp
@@ -1,10 +1,10 @@
 /*!
- *  \file File componentText.cpp
- *  \brief Implementation of the class of text components present in the game
+ *  @file File componentText.cpp
+ *  @brief Implementation of the class of text components present in the game
  *
  *  The class implemented here provides to the game the messeges elements
  *
- *  \sa componentText.hpp
+ *  @sa componentText.hpp
  */
 
 #include <componentText.hpp>
@@ -17,7 +17,7 @@
     /*!
     This is a constructor method of componentText class
     */
-CompText::CompText(const Text &t,Hotspot h,Vec2 p):txt{t},pos{p}{
+CompText::CompText(const Text &t, Hotspot h, Vec2 p):txt{t},pos{p}{
 	txt.set_hotspot(h);
 }
 
@@ -26,7 +26,7 @@ CompText::CompText(const Text &t,Hotspot h,Vec2 p):txt{t},pos{p}{
     This is a another constructor method of componentText class
 		with overload
     */
-CompText::CompText(string text,int size,SDL_Color c,Hotspot h,Vec2 p):txt{text,size,c},pos{p}{
+CompText::CompText(string text, int size, SDL_Color c, Hotspot h, Vec2 p):txt{text, size, c},pos{p}{
 	txt.set_hotspot(h);
 }
 
@@ -34,8 +34,9 @@ CompText::CompText(string text,int size,SDL_Color c,Hotspot h,Vec2 p):txt{text,s
     /*!
       This is a destructor method of componentText class
     */
+CompText::~CompText() {
 
-CompText::~CompText() {}
+}
 
 /*!
 	@fn void CompMovement::Update(float time)
@@ -44,7 +45,6 @@ CompText::~CompText() {}
 	@return The execution of this method returns no value
 	@warning Method that requires review of comment
 */
-
 void CompText::update(float time) {
 	UNUSED(time);
 }
@@ -55,13 +55,17 @@ void CompText::update(float time) {
 	@return The execution of this method returns no value
 	@warning Method that requires review of comment
 */
-
 void CompText::render() {
+
 	Vec2 p = pos + GO(entity)->Box().corner();
 	txt.SetPos(p);
 
-	if (GO(entity)->anchored) txt.Render();
-	else txt.Render(CAMERA);
+	if (GO(entity)->anchored) {
+		txt.Render();
+	}
+		else {
+		txt.Render(CAMERA);
+	}
 }
 
 /*!
